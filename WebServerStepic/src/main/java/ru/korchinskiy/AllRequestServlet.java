2b88c409b5ffe3b88be9c096b00387e5950a64cc
IlyaKorchinskiy <korchinskiy.ilya@gmail.com>
2018-10-30 10:22:30 +0300

    pageGEnerator

---
+++ WebServerStepic/src/main/java/ru/korchinskiy/AllRequestServlet.java
@@ -0,0 +1,33 @@
+package ru.korchinskiy;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AllRequestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Map<String, Object> pageVariables = createPageVariablesMap(request);
+        pageVariables.put("message", "");
+
+        response.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
+
+
+    }
+
+    private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
+        Map<String, Object> pageVariables = new HashMap<String, Object>();
+        pageVariables.put("method", request.getMethod());
+        pageVariables.put("URL", request.getRequestURL().toString());
+        pageVariables.put("pathInfo", request.getPathInfo());
+        pageVariables.put("sessionId", request.getSession().getId());
+        pageVariables.put("parameters", request.getParameterMap().toString());
+        return pageVariables;
+    }
+}
---
+++ WebServerStepic/src/main/java/ru/korchinskiy/MainClass.java
@@ -0,0 +1,7 @@
+package ru.korchinskiy;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+    }
+}
